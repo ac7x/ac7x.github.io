517bece43262b31975cfcc1e6fd5d79549f0e67c
ac7x <ac7x@pm.me>
2025-04-03 15:12:07 +0800

    Initial commit

--- webpanel/next.config.js
+++ webpanel/next.config.js
@@ -1,10 +1,8 @@
+// next.config.js (CommonJS)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',  // 如果需静态导出
   reactStrictMode: true,
-}
+};
 
 module.exports = nextConfig;
